3a9d9ec2ebc71293092d5730458aeb7d55a9a88e
Valentina <valentinaolmosoo@gmail.com>
2026-05-03 16:47:27 -0300

    footer agregado

--- portafolio-mtp/src/components/Footer.jsx
+++ portafolio-mtp/src/components/Footer.jsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">
-        Maria Teresa Pincheira © 2026 · Todos los derechos reservados | Powered by{" "}
-        <a
+        Maria Teresa Pincheira © 2026 · Todos los derechos reservados | Desarrollado por{" "}
+         <a
           href="https://valentinaolmos-portafolio.netlify.app/#projects"
           target="_blank"
           rel="noopener noreferrer"
